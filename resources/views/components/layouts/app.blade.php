dd4ea40403d11ec6075b52dd8b2e8ed8e2371098
Arthur <arthur6229@gmail.com>
2025-12-04 15:46:51 +0100

    ajt ligne

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,6 +8,8 @@
         
         <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;600;700&display=swap" rel="stylesheet">
         <style>body { font-family: 'Fredoka', sans-serif; }</style>
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
       
     </head>
