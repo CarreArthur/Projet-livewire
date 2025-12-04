fa3a3a96e6e4f135fdae0aa72ce152e8a5986b01
Arthur <arthur6229@gmail.com>
2025-12-04 15:25:14 +0100

    test

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'Burger Architect' }}</title>
+        
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;600;700&display=swap" rel="stylesheet">
+        <style>body { font-family: 'Fredoka', sans-serif; }</style>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         {{ $slot }}
